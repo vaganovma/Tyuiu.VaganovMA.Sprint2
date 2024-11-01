3b2678efcefafbe46d3baefc3a7eae273aae7f3a
Tyuiu.vaganovma@outlook.com <Tyuiu.vaganovma@outlook.com>
2024-11-01 22:06:28 +0500

    Task6

---
+++ Tyuiu.VaganovMA.Sprint2.Task6.V5.Lib/DataService.cs
@@ -0,0 +1,25 @@
+namespace Tyuiu.VaganovMA.Sprint2.Task6.V5.Lib;
+
+using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+public class DataService : ISprint2Task6V5
+{
+    public string FindCardValue(int value)
+    {        
+        switch (value)
+        {
+            case 6:return "шестерка ";
+            case 7:return "семерка ";
+            case 8:return "восьмерка";
+            case 9:return "девятка ";
+            case 10:return "десятка ";
+            case 11:return "валет ";
+            case 12:return "дама";
+            case 13:return "король ";
+            case 14:return "туз ";
+            default: throw new ArgumentException("некорректные данные"); ;
+        }
+        
+    }
+}
