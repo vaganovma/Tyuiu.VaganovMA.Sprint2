c4da48c1eea5196a84b057e64a85d26642120201
Tyuiu.vaganovma@outlook.com <Tyuiu.vaganovma@outlook.com>
2024-11-01 22:09:12 +0500

    Task6

--- Tyuiu.VaganovMA.Sprint2.Task6.V5.Lib/DataService.cs
+++ Tyuiu.VaganovMA.Sprint2.Task6.V5.Lib/DataService.cs
@@ -9,15 +9,15 @@ public class DataService : ISprint2Task6V5
     {        
         switch (value)
         {
-            case 6:return "шестерка ";
-            case 7:return "семерка ";
-            case 8:return "восьмерка";
-            case 9:return "девятка ";
-            case 10:return "десятка ";
-            case 11:return "валет ";
-            case 12:return "дама";
-            case 13:return "король ";
-            case 14:return "туз ";
+            case 6: return "шестерка ";
+            case 7: return "семерка ";
+            case 8: return "восьмерка";
+            case 9: return "девятка ";
+            case 10: return "десятка ";
+            case 11: return "валет ";
+            case 12: return "дама";
+            case 13: return "король ";
+            case 14: return "туз ";
             default: throw new ArgumentException("некорректные данные"); ;
         }
         
