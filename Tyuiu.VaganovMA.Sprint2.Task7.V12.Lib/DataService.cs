63fe608e1a68ed0895d7a5897ccba411a4f22057
Tyuiu.vaganovma@outlook.com <Tyuiu.vaganovma@outlook.com>
2024-11-30 12:20:04 +0500

    Task6

--- Tyuiu.VaganovMA.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.VaganovMA.Sprint2.Task7.V12.Lib/DataService.cs
@@ -21,4 +21,16 @@ public class DataService : ISprint2Task7V12
         return res;
     }
 }
-
+int a = 2
+int b = 4
+int c = 3
+int d = 5
+        
+for(int i = 1; i<5; i++)
+{
+c++;
+c+=b;
+a=b+c;
+d=(a-b)+i
+}       
+Console.WriteLine(d);
